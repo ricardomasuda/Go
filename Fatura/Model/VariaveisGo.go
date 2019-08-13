6b5f16a02cc152e4d2a45f431efa328f30dc252c
ricardo <m-ricardo1611@hotmail.com>
2019-08-12 22:40:34 -0300

    tirar

--- Fatura/Model/VariaveisGo.go
+++
@@ -1,9 +0,0 @@
-package Model
-
-import "html/template"
-
-//Modelos armazena os modelos de pagina que serão executados pelos manipuladores
-var Login = template.Must(template.ParseFiles("Pag/login.html"))
-
-//Modelos armazena os modelos de pagina que serão executados pelos manipuladores
-var Fatura = template.Must(template.ParseFiles("Pag/fatura.html"))
