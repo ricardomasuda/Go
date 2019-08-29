6ad3cc3c94e2a0f5cf7dae4d27b2e9f66fb7c980
ricardo <m-ricardo1611@hotmail.com>
2019-08-28 22:30:50 -0300

    reinicio

--- Fatura/Controladores/VarialGoLocal.go
+++
@@ -1,7 +0,0 @@
-package Controladores
-
-import "net/http"
-
-func local(w http.ResponseWriter , r *http.Request) {
-
-}
--- Fatura/Model/Fatura.go
+++
@@ -1,7 +0,0 @@
-package Model
-
-type Fatura struct {
-	IdFatura int `json:"userID" db:"id_fatura"`
-	Valor string `json:"Nome" db:"valor"`
-	Categoria string `json:"Senha" db:"categoria"`
-}
