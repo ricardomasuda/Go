e9fff6dc3e521d71f6035e15de8c623a0532f9d4
ricardo <m-ricardo1611@hotmail.com>
2019-08-28 22:29:20 -0300

    novo inicio

--- Fatura/Model/VariaveisGo.go
+++
@@ -1,8 +0,0 @@
-package Model
-
-import "html/template"
-
-//Modelos armazena os modelos de pagina que serão executados pelos manipuladores
-var Modelos = template.Must(template.ParseFiles("Pag/login.html"))
-
-
--- Fatura/go.mod
+++
@@ -1,5 +0,0 @@
-module Fatura
-
-go 1.12
-
-require github.com/jeffprestes/cursodego v0.0.0-20180803140132-05d464c9be73 // indirect
